bf1514ea52f1e1af9a14934c0757057a20b7a2cf
Edmund Noble <edmundnoble@gmail.com>
2019-01-22 03:33:05 -0500

    Show instances, main depends on bound now

--- src/debruijn-lam-tree/LamTreeB.hs
+++ src/debruijn-lam-tree/LamTreeB.hs
@@ -1,3 +1,6 @@
+{-# language EmptyDataDeriving #-}
+{-# language DerivingStrategies #-}
+{-# language GeneralizedNewtypeDeriving #-}
 {-# language TypeFamilies #-}
 {-# language PolyKinds #-}
 
@@ -10,15 +13,14 @@ module LamTreeB(
 
 import Bound.Scope
 import Control.Monad.Trans.Identity
-import Data.Functor.Const
 import Data.Void
 
 import Common
 
-newtype VarAST v (lt :: * -> *) = VarAST v
-newtype LamAST v (lt :: * -> *) = LamAST (Scope v lt String)
-newtype LitAnn (v :: *) (lt :: * -> *) = LitAnn ()
-data Extra (v :: *) (lt :: * -> *)
+newtype VarAST v (lt :: * -> *) = VarAST v deriving newtype Show
+newtype LamAST v (lt :: * -> *) = LamAST (Scope v lt String) deriving newtype Show
+newtype LitAnn (v :: *) (lt :: * -> *) = LitAnn () deriving newtype Show
+data Extra (v :: *) (lt :: * -> *) deriving Show
 -- self-recursive!
-newtype AppArgAST (v :: *) lt = AppArgAST (lt v)
-newtype AppFunAST (v :: *) lt = AppFunAST (lt v)
+newtype AppArgAST (v :: *) lt = AppArgAST (lt v) deriving newtype Show
+newtype AppFunAST (v :: *) lt = AppFunAST (lt v) deriving newtype Show
