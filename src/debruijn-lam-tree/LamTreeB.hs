1801eccacd65c15cc983dad1e2cd767eb0839408
Edmund Noble <edmundnoble@gmail.com>
2019-01-22 03:22:12 -0500

    Delete wrong comment

--- src/debruijn-lam-tree/LamTreeB.hs
+++ src/debruijn-lam-tree/LamTreeB.hs
@@ -18,9 +18,6 @@ import Common
 newtype VarAST v (lt :: * -> *) = VarAST v
 newtype LamAST v (lt :: * -> *) = LamAST (Scope v lt String)
 newtype LitAnn (v :: *) (lt :: * -> *) = LitAnn ()
--- we could also leave this uninstantiated; we won't be able to write instances
--- that touch it, but later if we want to assign to LamTreeX the rest of our
--- code will still work!
 data Extra (v :: *) (lt :: * -> *)
 -- self-recursive!
 newtype AppArgAST (v :: *) lt = AppArgAST (lt v)
--- src/naive-lam-tree/LamTreeN.hs
+++ src/naive-lam-tree/LamTreeN.hs
@@ -19,9 +19,6 @@ newtype Scope f a = Scope (f (Incr a))
 newtype VarAST v (lt :: * -> *) = VarAST v
 newtype LamAST v (lt :: * -> *) = LamAST (lt v)
 newtype LitAnn (v :: *) (lt :: * -> *) = LitAnn ()
--- we could also leave this uninstantiated; we won't be able to write instances
--- that touch it, but later if we want to assign to LamTreeX the rest of our
--- code will still work!
 data Extra (v :: *) (lt :: * -> *)
 -- self-recursive!
 newtype AppArgAST (v :: *) lt = AppArgAST (lt v)
